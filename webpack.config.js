9981aad147b4d139f5885b7004b93ec0373f04e7
Piwko Marcin <piwkoq@gmail.com>
2019-08-16 23:58:36 +0200

    Added new loaders and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: [
@@ -15,6 +17,16 @@ module.exports = {
 
     module: {
         rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
             {
                 test: /\.(sass|scss|css)$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
@@ -34,12 +46,24 @@ module.exports = {
     },
 
     plugins: [
-        new HtmlWebpackPlugin({
-            template:  "./src/index.html"
-        }),
         new MiniCssExtractPlugin({
             template: "./src/assets/sass/style.sass",
             filename: 'style.css'
         }),
-    ]
+    ],
+
+    optimization: {
+        minimizer: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            }),
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
 };
