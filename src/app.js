4d28f42819bbf167ca321c0dc7b6a18ecf8d2efe
Piwko Marcin <piwkoq@gmail.com>
2019-08-16 19:54:35 +0200

    Added listener on menu button

--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ import './assets/sass/style.sass';
 const playButton = document.querySelector('.play');
 const howToButton = document.querySelector('.how');
 const closeButton = document.querySelector('.close');
+const menuButton = document.querySelector('.menu');
 
 const rulesPage = document.querySelector('.rules');
 const gamePage = document.querySelector('.game');
+const gameResult = document.querySelector('.results');
 
 const showGame = () => {
     gamePage.classList.add('active');
     setTimeout(animButton, 1000);
+};
+
+const showResults = () => {
+    gameResult.classList.toggle('active');
+    menuButton.classList.toggle('color')
 };
 
 const showRules = () => {
@@ -18,15 +25,18 @@ const showRules = () => {
 };
 
 const animButton = () => {
-    closeButton.classList.add('show')
+    closeButton.classList.add('show');
+    menuButton.classList.add('show');
 };
 
 const closeWindow = () => {
     rulesPage.classList.remove('active');
     gamePage.classList.remove('active');
     closeButton.classList.remove('show');
+    menuButton.classList.remove('show');
 };
 
+menuButton.addEventListener('click', showResults);
 closeButton.addEventListener('click', closeWindow);
 howToButton.addEventListener('click', showRules);
 playButton.addEventListener('click', showGame);
